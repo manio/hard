manio/hard

commit 9ee02914310ae74670c25bd4e7784ddb2f8ec259

onewire: cosmetics: make a duration once in prolonging code

src/onewire.rs
@@ -1343,15 +1343,16 @@ impl OneWire {
                                                                                             } else {
                                                                                                 let toggled_elapsed = relay.last_toggled.unwrap_or(Instant::now()).elapsed();
                                                                                                 let mut prolong_secs = relay.pir_hold_secs;
+                                                                                                let d = Duration::from_secs_f32(prolong_secs);
                                                                                                 if relay.override_mode {
                                                                                                     if DEFAULT_PIR_PROLONG_SECS > relay.pir_hold_secs {
                                                                                                         prolong_secs = DEFAULT_PIR_PROLONG_SECS
                                                                                                     };
                                                                                                     if relay.switch_hold_secs > prolong_secs && toggled_elapsed > Duration::from_secs_f32(relay.switch_hold_secs - prolong_secs) {
-                                                                                                        relay.stop_after = Some(toggled_elapsed.add(Duration::from_secs_f32(prolong_secs)));
+                                                                                                        relay.stop_after = Some(toggled_elapsed.add(d));
                                                                                                     }
                                                                                                 } else {
-                                                                                                    relay.stop_after = Some(toggled_elapsed.add(Duration::from_secs_f32(prolong_secs)));
+                                                                                                    relay.stop_after = Some(toggled_elapsed.add(d));
                                                                                                 }
                                                                                                 info!(
                                                                                                     "{}: PIR prolonging: {}: bit={}, duration added: {}",
@@ -1361,7 +1362,7 @@ impl OneWire {
                                                                                                     ),
                                                                                                     relay.name,
                                                                                                     i,
-                                                                                                    format_duration(Duration::from_secs_f32(prolong_secs)),
+                                                                                                    format_duration(d),
                                                                                                 );
                                                                                             }
                                                                                         }
@@ -1486,20 +1487,21 @@ impl OneWire {
                                                                                 } else {
                                                                                     let toggled_elapsed = yeelight.last_toggled.unwrap_or(Instant::now()).elapsed();
                                                                                     let mut prolong_secs = yeelight.pir_hold_secs;
+                                                                                    let d = Duration::from_secs_f32(prolong_secs);
                                                                                     if yeelight.override_mode {
                                                                                          if DEFAULT_PIR_PROLONG_SECS > yeelight.pir_hold_secs {
                                                                                              prolong_secs = DEFAULT_PIR_PROLONG_SECS
                                                                                          };
                                                                                          if yeelight.switch_hold_secs > prolong_secs && toggled_elapsed > Duration::from_secs_f32(yeelight.switch_hold_secs - prolong_secs) {
-                                                                                            yeelight.stop_after = Some(toggled_elapsed.add(Duration::from_secs_f32(prolong_secs)));
+                                                                                            yeelight.stop_after = Some(toggled_elapsed.add(d));
                                                                                         }
                                                                                     } else {
-                                                                                        yeelight.stop_after = Some(toggled_elapsed.add(Duration::from_secs_f32(prolong_secs)));
+                                                                                        yeelight.stop_after = Some(toggled_elapsed.add(d));
                                                                                     }
                                                                                     info!(
                                                                                         "Yeelight: prolonging: {}, duration added: {}",
                                                                                         yeelight.name,
-                                                                                        format_duration(Duration::from_secs_f32(prolong_secs)),
+                                                                                        format_duration(d),
                                                                                     );
                                                                                 }
                                                                             }

commit 998e74e03f644ff415997a1dfb3fb434fc21a10f

onewire: fix regression caused in 9ee0291
The problem is that Duration created earlier doesn't take
into account a further prolong_secs computation.

src/onewire.rs
@@ -204,10 +204,11 @@ impl Device {
             } else {
                 let toggled_elapsed = self.last_toggled.unwrap_or(Instant::now()).elapsed();
                 let mut prolong_secs = self.pir_hold_secs;
-                let d = Duration::from_secs_f32(prolong_secs);
+                let mut d = Duration::from_secs_f32(prolong_secs);
                 if self.override_mode {
                     if DEFAULT_PIR_PROLONG_SECS > prolong_secs {
-                        prolong_secs = DEFAULT_PIR_PROLONG_SECS
+                        prolong_secs = DEFAULT_PIR_PROLONG_SECS;
+                        d = Duration::from_secs_f32(prolong_secs);
                     };
                     if self.switch_hold_secs > prolong_secs
                         && toggled_elapsed