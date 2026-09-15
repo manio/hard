801c16cf13991a5890fcc8e9dca35e649aec8c3c
Mariusz Bialonczyk <manio@skyboo.net>
2026-09-15 08:52:45 +0200

    bus delay: 1ms tests

--- src/onewire.rs
+++ src/onewire.rs
@@ -1844,7 +1844,7 @@ fn sensor_poller_thread(
             }
             // Throttle consecutive sysfs/w1 reads. The 1-Wire protocol timing
             // itself is handled by the kernel w1 master driver.
-            thread::sleep(Duration::from_micros(50));
+            thread::sleep(Duration::from_millis(1));
         }
     }
     info!("{}: sensor poller thread stopped", name);
